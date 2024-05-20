c28c2418b456a4d10f767cc90471e632742c1a36
Amir Aloui <amiraloui20@gmail.com>
2024-05-20 01:58:41 +0100

    add scoring routes

--- src/routes/v1/gameSession.routes.ts
+++ src/routes/v1/gameSession.routes.ts
@@ -1,20 +1,29 @@
-import express from 'express';
+import express from 'express'
 import {
   checkGameSessionStatus,
   createGameSession,
-} from '../../controllers/gameSession.controller';
-import validate from '../../middleware/validation.middleware';
-import { createGameSessionSchema } from '../../validations/gameSession.validations';
-import { asyncMiddleware } from '../../middleware/async.middleware';
+  getPlayerData,
+  updatePlayerScore,
+} from '../../controllers/gameSession.controller'
+import validate from '../../middleware/validation.middleware'
+import { createGameSessionSchema } from '../../validations/gameSession.validations'
+import { asyncMiddleware } from '../../middleware/async.middleware'
 
-const router = express.Router();
+const router = express.Router()
 
 router.post(
   '/:quizId',
   validate(createGameSessionSchema),
-  asyncMiddleware(createGameSession),
-);
+  asyncMiddleware(createGameSession)
+)
 
-router.get('/game-sessions/status', asyncMiddleware(checkGameSessionStatus));
+router.get('/game-sessions/status', asyncMiddleware(checkGameSessionStatus))
 
-export { router as gameSessionRoutes };
+router.put('/:gameSessionId/score', asyncMiddleware(updatePlayerScore))
+
+router.get(
+  '/:gameSessionId/player/:playerSessionId',
+  asyncMiddleware(getPlayerData)
+)
+
+export { router as gameSessionRoutes }
--- src/services/gameSession.service.ts
+++ src/services/gameSession.service.ts
@@ -1,22 +1,23 @@
-import { GAME_SESSION_ERRORS } from '../constants/errorMessages.constants';
-import NotFoundError from '../errors/not-found.error';
-import { GameSession, IGameSession } from '../models/gameSession.model';
-import { Quiz } from '../models/quiz.model';
+import { GAME_SESSION_ERRORS } from '../constants/errorMessages.constants'
+import BadRequestError from '../errors/bad-request.error'
+import NotFoundError from '../errors/not-found.error'
+import { GameSession, IGameSession } from '../models/gameSession.model'
+import { Quiz } from '../models/quiz.model'
 
 export default class GameSessionService {
   async createGameSession(
     quizId: string,
     moderatorSessionId: string,
-    playersSessionIds: string[],
+    playersSessionIds: string[]
   ): Promise<IGameSession> {
-    const quiz = await Quiz.findById(quizId);
-    if (!quiz) throw new NotFoundError(GAME_SESSION_ERRORS.NOT_FOUND);
+    const quiz = await Quiz.findById(quizId)
+    if (!quiz) throw new NotFoundError(GAME_SESSION_ERRORS.NOT_FOUND)
 
     const players = playersSessionIds.map((sessionId) => ({
       sessionId,
       score: 0,
       perks: [], // FIXME:
-    }));
+    }))
 
     const gameSession = await GameSession.create({
       quizId,
@@ -25,9 +26,57 @@ export default class GameSessionService {
       currentQuestionIndex: 0,
       selectedAnswers: [],
       correctAnswerIndices: [],
-    });
+    })
 
-    return gameSession;
+    return gameSession
+  }
+
+  async incrementPlayerScore(
+    playerSessionId: string,
+    gameSessionId: string
+  ): Promise<void> {
+    try {
+      const gameSession = await GameSession.findById(gameSessionId)
+      if (!gameSession) throw new Error('Game session not found')
+
+      const playerIndex = gameSession.players.findIndex(
+        (player) => player.sessionId === playerSessionId
+      )
+      if (playerIndex === -1)
+        throw new BadRequestError({
+          error: 'PLAYER_NOT_FOUND',
+          message: 'PLAYER_NOT_FOUND',
+        })
+
+      gameSession.players[playerIndex].score++
+      await gameSession.save()
+    } catch (error) {
+      console.error('Error updating player score:', error)
+      throw error
+    }
+  }
+
+  async findGameSession(gameSessionId: string): Promise<IGameSession> {
+    const gameSession = await GameSession.findById(gameSessionId)
+    if (!gameSession) {
+      throw new NotFoundError(GAME_SESSION_ERRORS.NOT_FOUND)
+    }
+    return gameSession
+  }
+
+  async getPlayerData(gameSessionId: string, playerSessionId: string) {
+    const gameSession = await this.findGameSession(gameSessionId)
+    const player = gameSession.players.find(
+      (player) => player.sessionId === playerSessionId
+    )
+
+    if (!player)
+      throw new NotFoundError({
+        message: 'Player not found in game session',
+        error: 'PLAYER_NOT_FOUND',
+      })
+
+    return player
   }
 
   // async submitAnswer(
